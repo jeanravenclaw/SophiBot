34bd00941d203116052e01371313b50e9ee09cac
Jean Ravenclaw <>
2021-05-06 06:57:03 +0000

    push stuff to vscode

--- cogs/data.py
+++ cogs/data.py
@@ -9,6 +9,11 @@ class Data(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def add_value(self, id, key):
+		""" creates a default value if a user's value doesn't exist """
+		keys = db.keys()
+
+
     @commands.command(name='dbset')
     async def dbset(self, ctx, *, anything):
         """Sets your personal value"""
@@ -18,11 +23,17 @@ class Data(commands.Cog):
     @commands.command(name='dbget')
     async def dbget(self, ctx):
         """Gets your personal value"""
-        value = db[str(ctx.author.id)]
-        if value != None:
-            await ctx.send(f'Here\'s your data: ```{value}```')
+        keys = db.keys() # lists all the keys
+        if ctx.author.id not in keys: 
+			# sets a value to your id
+            db[str(ctx.author.id)] = {
+				"data": ""
+			} 
         else:
-            await ctx.send(f'Your data doesn\'t exist!')
+            value = db[str(ctx.author.id)] # gets your value
+            if value != None:
+                await ctx.send(f'Here\'s your data: ```{value}```')
+		
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
