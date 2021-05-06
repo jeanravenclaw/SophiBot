49beceb4f8cc998871b7afcc88ff5e97d25bc29e
Jean Ravenclaw <>
2021-05-06 08:19:16 +0000

    dbget and dbset bug fixes

--- cogs/data.py
+++ cogs/data.py
@@ -4,50 +4,73 @@ import discord
 from replit import db
 from discord.ext import commands
 
+def value_check(id, key):
+        """ creates a default value if a user's value doesn't exist 
+        id is your user id, and key is the key you're looking for (eg. bank)"""
+
+        db_keys = db.keys() # lists all the database keys
+
+        if id not in db_keys: # checks if user id isn't on database
+            db[str(id)] = {} # gives your id a value
+            print("added user on database")
+        else:
+            print("user database check 1 completed")
+        # this makes sure that errors won't happen
+
+        id_value = db[str(id)] # easier to read your id's value
+
+        if key not in id_value: # checks if key isn't on your id
+            id_value[key] = "" # adds a key into your id
+            print(f"added key {key} to user")
+        else:
+            print("user database check 2 completed")
+        # this also makes sure that errors won't happen
 
 class Data(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def value_check(self, id, key):
-		""" creates a default value if a user's value doesn't exist 
-        id is your user id, and key is the key you're looking for (eg. bank)"""
-
-		db_keys = db.keys() # lists all the database keys
-        id_value = db[str(id)] # easier to read your id's value
-
-        if id not in db_keys: # checks if user id isn't on database
-            id_value = {} # gives your id a value
-        elif type(id) != dict # checks if user id's type isn't a dictionary
-            id_value = {} # makes your value a dictionary
-        # this makes sure that errors won't happen
-
-        if key not in id_value: # checks if key isn't on your id
-            id_value[key] = "" # adds a key into your id
-        # this also makes sure that errors won't happen
-
-
+    @commands.command(name="reset")
+    async def reset(self, ctx):
+        """ resets the database """
+        id = ctx.author.id
+        if id == 690173341104865310:
+            keys = db.keys()
+            for i in keys:
+                del db[i]
+                await ctx.send(f'deleted key {i}')
+                print(f"deleted key {i}")
+            await ctx.send(f'completed reset!')
+        else:
+            print(f"attempted reset by {id}")
 
     @commands.command(name='dbset')
     async def dbset(self, ctx, *, anything):
         """Sets your personal value"""
         id = str(ctx.author.id) # gets user id
-        value_check(id, data) # avoid errors
+        value_check(id, "data") # avoid errors
         id_db = db[id] # gets user's database
+        print("successfully set variables")
 
-        id_db[data] = str(anything) # sets 'data' in user's database
+        id_db["data"] = str(anything) # sets 'data' in user's database
         await ctx.send(f'Changed your data to: ```{anything}```')
+        print("successfully set data")
 
     @commands.command(name='dbget')
     async def dbget(self, ctx):
         """Gets your personal value"""
         id = str(ctx.author.id) # gets user id
-        value_check(id, data) # avoid errors
+        value_check(id, "data") # avoid errors
         id_db = db[id] # gets user's database
+        print("successfully set variables")
 
-        value = id_db[data] # gets 'data' in user's database
-        await ctx.send(f'Here\'s your data: ```{value}```')
-		
+        value = id_db["data"] # gets 'data' in user's database
+        if value != "":
+            await ctx.send(f'Here\'s your data: ```{value}```')
+        else:
+            await ctx.send(f"Your data is nonexistent!")
+        print("successfully set data")
+        
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
